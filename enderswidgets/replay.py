4da5047dd7e6907598e595d8f0eb5d87d4cc93e9
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-10-09 15:22:38 -0400

    impr: make sinks optional

--- enderswidgets/replay.py
+++ enderswidgets/replay.py
@@ -5,15 +5,27 @@ from enderswidgets.streams import StreamPoint, Prediction
 from enderswidgets.visualization import TimeSeriesVisualizer
 
 
-def replay(streams: Iterable[Iterable[dict]], horizon: int):
+class NoOp:
+    def process(self, *args, **kwargs):
+        pass
+
+def replay(streams: Iterable[Iterable[dict]], horizon: int, with_accounting: bool = True, with_visualization: bool = False):
+    """
+    Replay a set of streams and visualize the results.
+    :param streams:
+    :param horizon:
+    :param with_accounting:
+    :param with_visualization:
+    :return:
+    """
     try:
         from __main__ import infer
     except ImportError:
         print("Please define the 'infer' function in the main module.")
         return
-    accounting = AccountingDataVisualizer()
+    accounting = AccountingDataVisualizer() if with_accounting else NoOp()
     for stream_id, stream in enumerate(streams):
-        viz = TimeSeriesVisualizer()
+        viz = TimeSeriesVisualizer() if with_visualization else NoOp()
         prediction_generator = infer(stream, horizon)
         next(prediction_generator)
         for idx, data_point in enumerate(stream):
